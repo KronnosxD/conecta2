9ac4e96d9bb147ca36c09267d416b82777eeb40e
KronnosxD <asdpof@gmail.com>
2018-08-11 02:13:42 -0400

    ...

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 768, height: 768})
   
     // and load the index.html of the app.
     win.loadURL('http://localhost:4200/');
